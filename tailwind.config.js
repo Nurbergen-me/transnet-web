7e1b1ff22978256d20fd81e159380a0bb7b46e8b
nurik.eleshov <nurik.eleshov@gmail.com>
2023-01-30 23:51:33 +0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: {
+        blue: '#2E95D2',
+        white: '#fff'
+      },
+      secondary: {
+        burgerbg: 'rgba(255, 255, 255, 0.15)',
+        producttitle: 'rgba(24, 24, 24, 0.67)',
+        blue: '#256E9A',
+        gray: '#CCC',
+        quoteline: 'rgba(48, 48, 48, 0.2)'
+      }
+    }
+  },
+  plugins: [],
+}
